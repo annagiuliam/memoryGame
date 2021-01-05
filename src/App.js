a3b542cbf44354d8b67bca60f29a86297a796378
annagiuliam <annagiulia.musso@gmail.com>
2021-01-05 12:38:54 +0100

    finished game logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import {shuffle} from 'lodash';
 import catArr from './components/cats';
 import './App.css';
 import Card from './components/card';
@@ -7,46 +8,63 @@ import Header from './components/header';
 
 function App() {
   const [cats, setCats] = useState(catArr);
-  let  [score, setScore] = useState(0);
-  let [highScore, setHighScore] = useState(0);
+  const  [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+
+  useEffect(() => {
+    if (score === cats.length) { 
+      alert("You won!")
+      setHighScore(score)
+      setScore(0)
+      resetCats();
+      shuffleArray(cats);
+    }
+  })
 
   function handleClick(catId) {
     const clickedCat = cats.find((cat) => cat.id === catId);
-    if (clickedCat.clicked === false) { 
-      handleWin(clickedCat)
-    } else {
-      handleLose()
-    }
+    (! clickedCat.clicked ) ?  handleScore(clickedCat) :  handleLose();
+   
   }
 
-  function handleWin(clickedCat) {
+  function handleScore(clickedCat) {    
+    setScore(score + 1); 
     clickCat(clickedCat);
-    setScore(score + 1);
+    shuffleArray(cats);
+    
   }
 
   function handleLose() {
     if (score > highScore) {
-      setHighScore(highScore = score)
+      setHighScore(score) 
     }   
-    setScore(score = 0);
-    
+    setScore(0);
+    resetCats();
+    alert("You lost, try again!")
+    shuffleArray(cats);
   }
 
-  function clickCat(clickedCat) {   
-   clickedCat.clicked = true;
-   setCats([...cats]);
-   console.log(cats);
+  function resetCats() {
+    const newCats = cats.map((cat) => {
+      cat.clicked = false
+      return cat;
+    });
+    setCats(newCats)
   }
 
-  function handleScore(clickedCat) {
-    
+  function clickCat(clickedCat) { 
+    const newCats = cats.map((cat) => {
+      if (cat.id === clickedCat.id) {
+        cat.clicked = true;
+      }
+      return cat
+    })
+    setCats(newCats);
   }
 
   function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
+    const newArr = shuffle(array)
+    setCats(newArr);
   }
 
   return (
--- src/components/cats.js
+++ src/components/cats.js
@@ -1,7 +1,21 @@
 import cat1 from '../images/cat1.jpeg';
 import cat2 from '../images/cat2.jpeg';
+import cat3 from '../images/cat3.jpeg'
+import cat4 from '../images/cat4.jpeg'
+import cat5 from '../images/cat5.jpeg'
+import cat6 from '../images/cat6.jpeg'
+import cat7 from '../images/cat7.jpeg'
+import cat8 from '../images/cat8.jpeg'
+import cat9 from '../images/cat9.jpeg'
+import cat10 from '../images/cat10.jpeg'
+import cat11 from '../images/cat11.jpeg'
+import cat12 from '../images/cat12.jpeg'
 
 let catArr = [
+/* for (let i = 1; i<= 12; i++) {
+    let obj = {id : i, src : 'cat' + i, clicked : "false"};
+    catArr.push(obj)  
+} */
     {id : 1,
     src : cat1,
     clicked : false
@@ -9,6 +23,46 @@ let catArr = [
     
     {id : 2,
     src : cat2,
+    clicked : false
+    },
+    {id : 3,
+        src : cat3,
+        clicked : false
+    },
+    {id : 4,
+    src : cat4,
+    clicked : false
+    },
+    {id : 5,
+    src : cat5,
+    clicked : false
+    },
+    {id : 6,
+    src : cat6,
+    clicked : false
+    },
+    {id : 7,
+    src : cat7,
+    clicked : false
+    },
+    {id : 8,
+    src : cat8,
+    clicked : false
+    },
+    {id : 9,
+    src : cat9,
+    clicked : false
+    },
+    {id : 10,
+    src : cat10,
+    clicked : false
+    },
+    {id : 11,
+    src : cat11,
+    clicked : false
+    },
+    {id : 12,
+    src : cat12,
     clicked : false
     }
 ]
